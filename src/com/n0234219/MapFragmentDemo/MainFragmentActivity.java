16d6a1eefe2c25b084abec4402706c6690b4b8a3
Sam Hutchings <samhutchings@gmail.com>
2012-03-21 22:09:00 +0000

    prepare main fragment for passing data

--- src/com/n0234219/MapFragmentDemo/MainFragmentActivity.java
+++ src/com/n0234219/MapFragmentDemo/MainFragmentActivity.java
@@ -1,14 +1,33 @@
 package com.n0234219.MapFragmentDemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 
-public class MainFragmentActivity extends Activity {
-    
-    @Override    
-    protected void onCreate(Bundle bundle) {        
-        super.onCreate(bundle);
-        setContentView(R.layout.main_layout);        
-    }
+public class MainFragmentActivity extends Activity implements PhotoGridFragment.OnImageSelectedListener {
+
+	@Override    
+	protected void onCreate(Bundle bundle) {        
+		super.onCreate(bundle);
+		setContentView(R.layout.main_layout);        
+	}
+
+	public void onImageSelected(String lat, String lng) {
+		MapViewerFragment viewer = (MapViewerFragment) getFragmentManager()
+				.findFragmentById(R.id.mapview_fragment);
+
+		if (viewer == null || !viewer.isInLayout()) {
+			Intent showContent = new Intent(getApplicationContext(),
+					MapViewerActivity.class);
+			showContent.putExtra("latitude", lat);
+			showContent.putExtra("longitude", lng);
+			startActivity(showContent);	
+
+		} else {
+			viewer.updateMapPosition(lat, lng);
+		}
+
+
+	}
 }
